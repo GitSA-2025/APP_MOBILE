56c644a9eebe9cbddeb000494498c01ddcb57bd4
GitSA-2025 <accesssystemfatec.com>
2025-08-12 11:20:17 -0300

    2º commit

--- SA/src/screens/Login/index.js
+++ SA/src/screens/Login/index.js
@@ -1,11 +1,53 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import styles from './styles';
+import { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+
+import AnimatedInput from '../../components/AnimatedInput';
 
 export default function Login() {
+    const navigation = useNavigation();
+
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    const handleRegister = () => {
+        navigation.navigate('Register');
+    }
     return (
-        <View>
-            <Text>
-                Login
-            </Text>
+        <View style={styles.container}>
+            <View style={styles.leftPainel}>
+                <Text style={styles.title}>Olá, seja bem-vindo novamente!</Text>
+                <Text style={styles.subTitle}>Conecte-se à sua conta para continuar e utilizar as funcionalidades do APP!</Text>
+            </View>
+            <View style={styles.rightPainel}>
+                <Text style={styles.titleLogin}>Faça login no SA</Text>
+                <AnimatedInput
+                    label="Email"
+                    iconName="email"
+                    value={email}
+                    onChangeText={setEmail}
+                    keyboardType="email-address"
+                />
+                <AnimatedInput
+                    label="Senha"
+                    iconName="lock"
+                    value={senha}
+                    onChangeText={setSenha}
+                    secureTextEntry
+                />
+                <TouchableOpacity style={styles.btnLinkText}>
+                    <Text style={styles.linkText}>Esqueci a senha.</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.btnAvancar}>
+                    <Text style={styles.btnText}>Avançar</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={[styles.btnLinkText, {alignSelf: 'center', marginTop: 16}]} onPress={handleRegister}>
+                    <Text style={styles.linkText}>Não possuo login.</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
--- SA/src/screens/Register/index.js
+++ SA/src/screens/Register/index.js
@@ -1,11 +1,67 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
+import styles from './styles';
+import { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
 
+import AnimatedInput from '../../components/AnimatedInput';
 export default function Register() {
+    const navigation = useNavigation();
+
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const [repetirSenha, setRepetirSenha] = useState('');
+
+
+    const handleRegister = () => {
+        navigation.navigate('Login');
+    }
     return (
-        <View>
-            <Text>
-                Register
-            </Text>
+        <View style={styles.container}>
+            <View style={styles.leftPainel}>
+                <Text style={styles.title}>Olá, seja bem-vindo(a) novo usuário!</Text>
+                <Text style={styles.subTitle}>Crie a sua conta nova no SA para continuar e utilizar as funcionalidades do APP!</Text>
+            </View>
+            <ScrollView>
+                <View style={styles.rightPainel}>
+                    <Text style={styles.titleLogin}>Crie a sua conta</Text>
+                    <AnimatedInput
+                        label="Nome"
+                        iconName="account"
+                        value={nome}
+                        onChangeText={setNome}
+                    />
+                    <AnimatedInput
+                        label="Email"
+                        iconName="email"
+                        value={email}
+                        onChangeText={setEmail}
+                        keyboardType="email-address"
+                    />
+                    <AnimatedInput
+                        label="Senha"
+                        iconName="lock"
+                        value={senha}
+                        onChangeText={setSenha}
+                        secureTextEntry
+                    />
+                    <AnimatedInput
+                        label="Repita a senha"
+                        iconName="lock"
+                        value={repetirSenha}
+                        onChangeText={setRepetirSenha}
+                        secureTextEntry
+                    />
+
+                    <TouchableOpacity style={styles.btnAvancar}>
+                        <Text style={styles.btnText}>Avançar</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={[styles.btnLinkText, { alignSelf: 'center', marginTop: 16 }]} onPress={handleRegister}>
+                        <Text style={styles.linkText}>Já possuo login.</Text>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
         </View>
     );
 }
